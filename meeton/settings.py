9201ecef9870fa47e37cdf5f156a0b0dbbddab13
Ravhan Shafi <80163239+sayimshah@users.noreply.github.com>
2022-10-25 00:16:14 +0530

    aaa

--- meeton/settings.py
+++ meeton/settings.py
@@ -120,8 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
 STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
 # Default primary key field type
